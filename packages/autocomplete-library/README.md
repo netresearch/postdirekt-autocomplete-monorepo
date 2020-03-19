6c719cfd2ef1276e171fbb4e142036da22ba5eaf
Max Melzer <max.melzer@netresearch.de>
2020-03-19 11:42:40 +0100

    PDTOW-94: Use rollup-plugin-css-only to include styles.css in dist

--- packages/autocomplete-library/README.md
+++ packages/autocomplete-library/README.md
@@ -1,8 +1,42 @@
 # Deutsche Post Direkt DATAFACTORY Autocomplete Library
 
-## Installation
+## Manual Installation
 
-t.b.d.
+1. Generate the compiled JavaScript files via `yarn build` (see "Install Dependencies")
+2. Pick the format you need from `dist/browser/postdirekt-autocomplete-lib.*.js
+3. Move the `.js` file to your project
+4. Move the file `dist/browser/styles.css` to your project
+
+## Usage
+
+Include the library `.js` and `.css` on the HTML page where your form is located:
+
+``` html
+<head>
+    <script type="text/javascript"
+            src="path/to/postdirekt-autocomplete-lib.*.js"></script>
+    <link rel="stylesheet"
+        type="text/css"
+        href="path/to/styles.css">
+</head>
+```
+
+Instantiate the library, passing in the following information:
+
+- the `HTMLInputElement`s for your *street*, *city*, *postal code* and *country* inputs
+- the country input value that maps to "Germany"
+- a valid DATAFACTORY Autocomplete access token. You can generate a token from PHP with the [PHP Authentication SDK](https://github.com/netresearch?q=autocomplete)
+
+``` js
+postdirektAutocompleteLib.init(
+        document.querySelector('[id="shipping:street1"]'),
+        document.querySelector('[id="shipping:city"]'),
+        document.querySelector('[id="shipping:postcode"]'),
+        document.querySelector('[name="shipping[country_id]"]'),
+        'DE',
+        'YOUR_ACCESS_TOKEN'
+    );
+```
 
 ## Developer and Contribution Workflow
 
--- rollup.config.js
+++ rollup.config.js
@@ -3,36 +3,46 @@ import commonjs from '@rollup/plugin-commonjs';
 import sourceMaps from 'rollup-plugin-sourcemaps';
 import camelCase from 'lodash.camelcase';
 import typescript from 'rollup-plugin-typescript2';
+import css from 'rollup-plugin-css-only'
+
 const libraryName = 'postdirekt-autocomplete-lib';
 
-export default {
-    input: `packages/autocomplete-library/src/${libraryName}.ts`,
-    output: [
-        { file: `dist/browser/${libraryName}.umd.js`, name: camelCase(libraryName), format: 'umd', sourcemap: true },
-        { file: `dist/browser/${libraryName}.amd.js`, name: camelCase(libraryName), format: 'amd', sourcemap: true },
-        { file: `dist/browser/${libraryName}.es.js`, name: camelCase(libraryName), format: 'es', sourcemap: true },
-        { file: `dist/browser/${libraryName}.cjs.js`, name: camelCase(libraryName), format: 'cjs', sourcemap: true },
-        { file: `dist/browser/${libraryName}.min.js`, name: camelCase(libraryName), format: 'iife', sourcemap: true },
-        { file: `dist/browser/${libraryName}.system.js`, name: camelCase(libraryName), format: 'system', sourcemap: true },
-    ],
-    // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-    watch: {
-        include: 'src/**',
+export default [
+    {
+        input: `packages/autocomplete-library/src/${libraryName}.ts`,
+        output: [
+            { file: `dist/browser/${libraryName}.umd.js`, name: camelCase(libraryName), format: 'umd', sourcemap: true },
+            { file: `dist/browser/${libraryName}.amd.js`, name: camelCase(libraryName), format: 'amd', sourcemap: true },
+            { file: `dist/browser/${libraryName}.es.js`, name: camelCase(libraryName), format: 'es', sourcemap: true },
+            { file: `dist/browser/${libraryName}.cjs.js`, name: camelCase(libraryName), format: 'cjs', sourcemap: true },
+            { file: `dist/browser/${libraryName}.min.js`, name: camelCase(libraryName), format: 'iife', sourcemap: true },
+            { file: `dist/browser/${libraryName}.system.js`, name: camelCase(libraryName), format: 'system', sourcemap: true },
+        ],
+        // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+        watch: {
+            include: 'src/**',
+        },
+        plugins: [
+            // Allow node_modules resolution, so you can use 'external' to control
+            // which external modules to include in the bundle
+            // https://github.com/rollup/rollup-plugin-node-resolve#usage
+            resolve(),
+
+            // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+            commonjs({
+                include: 'node_modules/**'
+            }),
+            // Compile TypeScript files
+            typescript({ useTsconfigDeclarationDir: true, tsconfig: 'tsconfig.es5.json' }),
+
+            // Resolve source maps to the original source
+            sourceMaps(),
+        ],
     },
-    plugins: [
-        // Allow node_modules resolution, so you can use 'external' to control
-        // which external modules to include in the bundle
-        // https://github.com/rollup/rollup-plugin-node-resolve#usage
-        resolve(),
-
-        // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-        commonjs({
-            include: 'node_modules/**'
-        }),
-        // Compile TypeScript files
-        typescript({ useTsconfigDeclarationDir: true, tsconfig: 'tsconfig.es5.json' }),
-
-        // Resolve source maps to the original source
-        sourceMaps(),
-    ],
-}
+    {
+        input: 'packages/autocomplete-library/css/styles.css',
+        plugins: [
+            css({ output: 'dist/browser/styles.css' })
+        ]
+    }
+]
